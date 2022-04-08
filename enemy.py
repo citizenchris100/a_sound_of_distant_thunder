41579bff5193342aed6d95050801a18338c60227
Christopher Manning <cmanning@tryelevate.com>
2022-04-08 11:56:12 -0500

    added enemy getters and setters

--- enemy.py
+++ enemy.py
@@ -1,10 +1,87 @@
 class Enemy:
-    def __init__(self, health, defence, strength, gun_skill, luck):
-        self.name = ''
+    def __init__(self, en_name, health, defence, strength, gun_skill, luck):
+        self.name = en_name
         self.hp = health
         self.dp = defence
         self.inventory = []
         self.strength_attribute = strength
         self.gun_skill_attribute = gun_skill
+        self.equipped_gun = ""
+        self.equipped_melee = ""
+        self.equipped_armour = ""
         self.luck_attribute = luck
         self.happy = 0
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def get_health(self):
+        return self.hp
+
+    def set_health(self, new_hp):
+        self.hp = new_hp
+
+    def get_defence(self):
+        return self.dp
+
+    def set_defence(self, new_dp):
+        self.dp = new_dp
+
+    def get_inventory(self):
+        return self.inventory
+
+    def add_inventory(self, new):
+        self.inventory.append(new)
+
+    def del_inventory(self, item):
+        self.inventory.remove(item)
+
+    def get_strength(self):
+        return self.strength_attribute
+
+    def set_strength(self, update_strength):
+        self.strength_attribute = update_strength
+
+    def get_gun_skill(self):
+        return self.gun_skill_attribute
+
+    def set_gun_attribute(self, gun_skill):
+        self.gun_skill_attribute = gun_skill
+
+    def get_equipped_gun(self):
+        return self.equipped_gun
+
+    def set_equipped_gun(self, new_gun):
+        self.equipped_gun = new_gun
+
+    def get_equipped_melee(self):
+        return self.equipped_melee
+
+    def set_equipped_melee(self, new_melee):
+        self.equipped_melee = new_melee
+
+    def get_equipped_armour(self):
+        return self.equipped_armour
+
+    def set_equipped_armour(self, new_armour):
+        self.equipped_armour = new_armour
+
+    def get_luck(self):
+        return self.luck_attribute
+
+    def set_luck(self, update_luck):
+        self.luck_attribute = update_luck
+
+    def get_happy(self):
+        return self.happy
+
+    def set_happy(self, add, value):
+        if add:
+            self.happy = self.happy + value
+        else:
+            self.happy = self.happy - value
+
+
